7e7607d3b89ec250494b20e7ba54b5379157758b
insub4067 <vb15@kakao.com>
2021-12-29 11:13:50 +0900

    first day

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,19 +1,46 @@
 const url = window.location.host;
-
+const messageList = document.querySelector("ul");
+const messsageForm = document.querySelector("#message");
+const nicknameForm = document.querySelector("#nickname");
 const socket = new WebSocket(`ws://${url}`);
 
+// JS object => string and send to Backend
+function makeMessage(type, payload){
+    const msg = {type, payload}
+    return JSON.stringify(msg)
+}
+
+// 연결 확인
 socket.addEventListener('open', ()=>{
     console.log("CONNECTED TO SERVER");
 });
 
-socket.addEventListener('message', (message)=>{
-    console.log("Message : ", message.data)
-});
-
+// 연결이 끊어졌을 경우
 socket.addEventListener('close', () => {
     console.log("DISCONNECTED FROM SERVER");
 });
 
-setTimeout( () => {
-    socket.send('message from browser');
-}, 3000);
+// 메시지가 왔을 경우
+socket.addEventListener('message', (message) => {
+    const li = document.createElement("li")
+    li.innerText = message.data
+    console.log(li)
+    messageList.append(li)
+})
+
+function handleNicknameSubmit(event){
+    event.preventDefault()
+    const input = nicknameForm.querySelector('input')
+    socket.send(makeMessage("nickname", input.value));    
+    input.value=''
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = messsageForm.querySelector('input');
+    socket.send(makeMessage("message", input.value));
+    input.value=''
+}
+
+messsageForm.addEventListener("submit", handleMessageSubmit);
+nicknameForm.addEventListener('submit', handleNicknameSubmit)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import WebSocket from "ws";
-import http from "http";
+import http from "http"; 
 
 const app = express();
 
@@ -12,23 +12,41 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log("Listen on http://localhost:3000")
-
+ 
+// 서버 만들고
 const server = http.createServer(app);
+// 서버 합치기
 const wss = new WebSocket.Server({server});
 
+const sockets = []
+
+// 연결됐을 경우
 wss.on("connection", (socket) => {
 
+    sockets.push(socket)
+
+    socket['nickname'] = 'Anonymous'
+
+    // 연결 됐을때 출력
     console.log("CONNECTED TO BROWSER");
 
+    // 연결 끊겼을 경우
     socket.on("close", ()=>{
         console.log("DISCONNECTED FROM BROWSER");
     });
 
-    socket.on("message", (message) => {
-        console.log("Message : ", message.toString());
+    // 메시지가 왔을 경우
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg)
+        switch (message.type){
+            case "message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${message.payload}`))
+                break
+            case "nickname":
+                socket['nickname'] = message.payload
+                break
+        }
     });
-
-    socket.send("Hello World");
 
 });
 
